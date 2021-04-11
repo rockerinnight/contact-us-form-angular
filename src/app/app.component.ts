201f08960376ec7baa985ce007c40000c326a72d
Linh Doan Vu <doanvulinh3108@gmail.com>
2021-04-11 09:47:12 +0700

    completed basic template-driven-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+class ContactForm {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'form-contact-us';
+  form = new ContactForm();
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.form);
+  }
+
+  newMessage(): void {
+    for (const propNames in this.form) {
+      if (Object.prototype.hasOwnProperty.call(this.form, propNames)) {
+        let propName = this.form['propName'];
+        propName = null;
+      }
+    }
+    // this.form = null;
+  }
 }
